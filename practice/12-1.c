a6fd4d938719cdefc14efe3d4a08b4a442248bb8
whistleloudly <hy1026419015@gmail.com>
2020-09-29 21:19:05 +0800

    12-1

---
+++ practice/12-1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    FILE *fp;
+    char ch,filename[10];
+    printf("Please enter the file name:");
+    scanf("%s",filename);
+    if((fp=fopen(filename,"w"))==NULL){
+        printf("Unable to open this file\n");
+        exit(0);
+    }
+    ch = getchar();
+    printf("Please enter a string  in the disk（Ends with a #）：");
+    ch = getchar();
+    while(ch!='#'){
+        fputc(ch,fp);
+        putchar(ch);
+        ch = getchar();
+    }
+    fclose(fp);
+    putchar(10);
+    return 0;
+}
